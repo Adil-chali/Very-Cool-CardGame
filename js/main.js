403287ffc5578068cdad6c479e72f52721566715
Adil Chali <wert-159@outlook.com>
2025-05-07 11:07:18 +0100

    added the logic of the game and showed cards images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const playerOneCard= document.getElementById("pOne")
+const playerTwoCard= document.getElementById("pTwo")
+const winnerWinner= document.querySelector("h2")
 let deckId=""
 fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 .then(res=>res.json())
@@ -15,6 +18,32 @@ function drawTwoCards() {
     .then(res=>res.json())
     .then(data=>{
         console.log(data);
+        playerOneCard.src=data.cards[0].image
+        playerTwoCard.src=data.cards[1].image
+        let player1Val= converteToNum(data.cards[0].value)
+        let player2Val= converteToNum(data.cards[1].value)
         
+        if (player1Val>player2Val) {
+            winnerWinner.textContent="PLAYER 1 WON"
+        }else if (player1Val<player2Val) {
+            winnerWinner.textContent="PLAYER 2 WON"
+            
+        }else{
+            winnerWinner.textContent="ITS WAAAR!"
+            
+        }
     })
+}
+function converteToNum(val) {
+if (val==="KING") {
+    return 13
+} else if (val==="ACE") {
+    return 14
+} else if(val==="QUEEN") {
+    return 12
+}   else if (val==="JACK") {
+    return 11
+}else{
+    return Number(val)
+}
 }
