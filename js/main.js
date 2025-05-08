2cca5c12d90f8ef9327f766bb59afb76a2f265b6
Adil Chali <wert-159@outlook.com>
2025-05-08 12:38:27 +0100

    Add localStorage to the game so players can continue from where they left
    -Save deck ID and last drawn cards
    - Restore game state on page reload
    - Clear storage on new game

--- js/main.js
+++ js/main.js
@@ -1,37 +1,59 @@
+document.getElementById("New-Game").addEventListener("click",startNewGame)
+document.getElementById("draw-cards").addEventListener("click",drawTwoCards)
 const playerOneCard= document.getElementById("pOne")
 const playerTwoCard= document.getElementById("pTwo")
 const winnerWinner= document.querySelector("h2")
 let deckId=""
-fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-.then(res=>res.json())
-.then(data=>{
-  deckId=data.deck_id
-    console.log(deckId);
+let winnerText=""
+if (localStorage.getItem("currentDeck")) {
+    deckId=localStorage.getItem("currentDeck")
+    playerOneCard.src=localStorage.getItem("p1card",)
+    playerTwoCard.src=localStorage.getItem("p2card")
+    winnerText= localStorage.getItem("lastWinner")
+    winnerWinner.textContent=winnerText
+}
+function startNewGame() {
     
-})
-.catch(err=>`error ${err}`)
-
-document.querySelector("button").addEventListener("click",drawTwoCards)
+    fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then(res=>res.json())
+    .then(data=>{
+      deckId=data.deck_id
+      console.log(deckId);
+      localStorage.clear()
+      localStorage.setItem("currentDeck",deckId)
+      playerOneCard.src=""
+      playerTwoCard.src=""
+      winnerWinner.textContent=""
+        
+    })
+    .catch(err=>`error ${err}`)
+}
 
 function drawTwoCards() {
     fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then(res=>res.json())
     .then(data=>{
         console.log(data);
+        localStorage.setItem("p1card",data.cards[0].image)
+        localStorage.setItem("p2card",data.cards[1].image)
+        localStorage.setItem("winner",data.cards[1].image)
         playerOneCard.src=data.cards[0].image
         playerTwoCard.src=data.cards[1].image
         let player1Val= converteToNum(data.cards[0].value)
         let player2Val= converteToNum(data.cards[1].value)
         
         if (player1Val>player2Val) {
+            winnerText="PLAYER 1 WON"
             winnerWinner.textContent="PLAYER 1 WON"
         }else if (player1Val<player2Val) {
-            winnerWinner.textContent="PLAYER 2 WON"
+            winnerText="PLAYER 2 WON"
             
         }else{
-            winnerWinner.textContent="ITS WAAAR!"
+            winnerText="ITS WAAAR!"
             
         }
+        winnerWinner.textContent=winnerText
+        localStorage.setItem("lastWinner", winnerText)
     })
 }
 function converteToNum(val) {
